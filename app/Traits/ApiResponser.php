14a3944b002e0e1be9c20cef8a68757223593858
archcos <arjay.charcos25@gmail.com>
2022-06-26 22:27:31 +0900

    edit

--- app/Traits/ApiResponser.php
+++ app/Traits/ApiResponser.php
@@ -9,10 +9,10 @@ trait ApiResponser{
 
 
     public function successResponse($data, $code = Response::HTTP_OK){
-        return response()->json(['data'=>$data,'site' => 1 ], $code);
+        return response()->json(['data'=>$data,'site' => 2 ], $code);
     }
 
     public function errorResponse($message, $code){
-        return response()->json(['error'=>$message, 'code'=>$code,'site' => 1 ], $code);
+        return response()->json(['error'=>$message, 'code'=>$code,'site' => 2 ], $code);
     }
 }
